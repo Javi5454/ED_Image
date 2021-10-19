57d5633f8fca5196b694922ae4cdbf4a030e9ae0
Javi5454 <javier-gomezlopez@outlook.es>
2021-10-19 17:52:35 +0200

    a

--- cmake-build-debug/CMakeFiles/prueba_zoom.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/prueba_zoom.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/adrian/CLionProjects/ED_Image/estudiante/src/prueba_zoom.cpp" "/home/adrian/CLionProjects/ED_Image/cmake-build-debug/CMakeFiles/prueba_zoom.dir/estudiante/src/prueba_zoom.cpp.o"
+  "/home/javi5454/Desktop/Github/ED_Image/estudiante/src/prueba_zoom.cpp" "/home/javi5454/Desktop/Github/ED_Image/cmake-build-debug/CMakeFiles/prueba_zoom.dir/estudiante/src/prueba_zoom.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
@@ -23,7 +23,7 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/adrian/CLionProjects/ED_Image/cmake-build-debug/CMakeFiles/image.dir/DependInfo.cmake"
+  "/home/javi5454/Desktop/Github/ED_Image/cmake-build-debug/CMakeFiles/image.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
